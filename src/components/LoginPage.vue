adbea4dab2c8dc084f023e979e791a8f2af2d05f
martentyrk <martentyrk@gmail.com>
2020-11-16 21:54:50 +0200

    Started working on Login page

---
+++ src/components/LoginPage.vue
@@ -0,0 +1,95 @@
+<template>
+   <section class="login-page">
+      <section id="login-container">
+        <div>
+          <h1>Welcome to postIt!</h1>
+        </div>
+        <div>
+          <h4>
+            <a href="#">Create an Account</a>
+          </h4>
+        </div>
+        <div>
+          <h4>Or</h4>
+        </div>
+        <div>
+          <h3>Please Log In</h3>
+          <form>
+            <div>
+              <input type="text" name="email" placeholder="Email">
+            </div>
+            <div>
+              <input type="password" name="password" placeholder="Password">
+            </div>
+            <div>
+              <button class="button" type="submit" name="login">Log In</button>
+            </div>
+            <div>
+              <small>
+                <a href="#">Forgot password?</a>
+              </small>
+            </div>
+          </form>
+        </div>
+      </section>
+    </section>
+</template>
+
+<script>
+export default {
+    name: "LoginPage",
+}
+</script>
+
+<style scoped>
+    @import url('https://fonts.googleapis.com/css2?family=Roboto+Slab&display=swap');
+    * {
+        font-family: "Roboto Slab", serif;
+        outline: none;
+        font-weight:bold;
+    }
+
+    html,
+    body {
+        margin: 0;
+        padding: 0;
+        width: 100%;
+        height: 100%;
+        background-color: #0277bd;
+        color: #263238;
+    }
+
+    body {
+        background-color: #0277bd;
+        color: #263238;
+    }
+
+    a {
+        color: #40c4ff;
+    }
+
+    .login-page {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    #login-container {
+        background-image: url("/images/logo.png");
+        background-size: 20% auto;
+        background-repeat: no-repeat;
+        background-position: 15px 15px;
+        min-width: 25%;
+        padding: 45px 45px;
+        background-color: #ffffff;
+        box-shadow: 0 0 15px rgba(38, 50, 56, 0.33);
+        text-align: center;
+    }
+
+    #login-container form div > input {
+        padding: 8px 16px;
+        margin: 4px 0;
+    }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,18 +2,20 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Browse from "../components/Browse.vue";
 import MainPage from "../components/MainPage.vue";
+import LoginPage from "../components/LoginPage.vue";
 
 // Load router
 Vue.use(VueRouter);
 
 // Specify which component to use on a route
 const routes = [
-    { path: "/browse", name: "Browse", component: Browse },
-    { path: "/", name: "MainPage", component: MainPage },
+  { path: "/browse", name: "Browse", component: Browse },
+  { path: "/login", name: "Login", component: LoginPage },
+  { path: "/", name: "MainPage", component: MainPage },
 ];
 
 export default new VueRouter({
-    mode: "history",
-    base: __dirname,
-    routes,
+  mode: "history",
+  base: __dirname,
+  routes,
 });
